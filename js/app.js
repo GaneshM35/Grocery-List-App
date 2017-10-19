860480d960f2b4664a91cd4a22124717e1015c3a
Ganesh M <ganesh.m635@gmail.com>
2017-10-19 19:18:52 +0530

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Ganesh M on 19/10/2017.
+ */
+var app = angular.module('groceryApp', ["groceryCtrlModule"]);
---
+++ js/controllers/groceryCtrl.js
@@ -0,0 +1,30 @@
+angular.module("groceryCtrlModule", [])
+
+.controller("GroceryCtrl",["$scope", "Calculator",function ($scope, Calculator){
+    $scope.grocObject =[];
+    $scope.grocObject.title = "Main Page";
+    $scope.grocObject.subTitle = "Sub Title";
+    $scope.grocObject.Firstname = "Ganesh";
+    $scope.grocObject.Lastname = "M";
+
+    $scope.grocObject.bindOutput = 2;
+
+    $scope.timesTwo = function () {
+        $scope.grocObject.bindOutput = Calculator.timesTwo($scope.grocObject.bindOutput);
+    }
+}])
+
+.directive("gmWelcomeMessage", function () {
+   return{
+       restrict: "E",
+       template: "<div>Thanks, Ganesh M</div>"
+   }
+})
+
+.factory("Calculator", function () {
+   var calculations = {};
+   calculations.timesTwo = function (a) {
+       return a * 2;
+   };
+   return calculations;
+});
